d17e72db5ecd6bfea0c2496b905dc8d64b05798d
jiingwang <jonneswong@gmail.com>
2017-10-30 18:53:18 +0800

    fea: 新增多项目属性

--- demo/components/page.vue
+++ demo/components/page.vue
@@ -1,43 +1,58 @@
 <template>
     <div class="page">
         <skeleton-loading>
-            <row :gutter="{top: '10px', bottom: '10px'}">
-                <column :span="24">
-                    <square-skeleton height="26px"></square-skeleton>
-                </column>
-            </row>
-            <row :gutter="{top: '0', bottom: '15px'}">
-                <column :span="24">
-                    <square-skeleton height="26px"></square-skeleton>
-                </column>
+            <row 
+                :gutter="{
+                    bottom: '15px'
+                }"
+            >
+                <square-skeleton 
+                    :count="2"
+                    :boxProperties="{
+                        top: '10px',
+                        height: '26px'
+                    }"
+                >
+                </square-skeleton>
             </row>
             <row>
                 <column :span="4">
                     <circle-skeleton></circle-skeleton>
                 </column>
                 <column :span="20" :gutter="20">
-                    <row :gutter="{top: '10px', bottom: '10px'}">
-                        <square-skeleton width="70px" height="15px"></square-skeleton>
-                    </row>
-                    <row :gutter="{bottom: '10px'}">
-                        <square-skeleton width="100px" height="15px"></square-skeleton>
-                    </row>
+                    <square-skeleton 
+                        :boxProperties="{
+                            top: '10px',
+                            width: '70px',
+                            height: '15px'
+                        }"
+                    >
+                    </square-skeleton>
+                    <square-skeleton 
+                        :boxProperties="{
+                            width: '100px',
+                            height: '15px',
+                            top: '10px'
+                        }"
+                    >
+                    </square-skeleton>
                 </column>
             </row>
-            <row :gutter="{top: '20px', bottom: '10px'}">
-                <square-skeleton></square-skeleton>
-            </row>
-            <row :gutter="{bottom: '10px'}">
-                <square-skeleton></square-skeleton>
-            </row>
-            <row :gutter="{bottom: '10px'}">
-                <square-skeleton></square-skeleton>
-            </row>
-            <row :gutter="{bottom: '10px'}">
-                <square-skeleton></square-skeleton>
-            </row>
-            <row :gutter="{bottom: '10px'}">
-                <square-skeleton height="200px"></square-skeleton>
+            <row :gutter="{top: '20px'}">
+                <square-skeleton 
+                    :count="4"
+                    :boxProperties="{
+                        bottom: '10px'
+                    }" 
+                >
+                </square-skeleton>
+                <square-skeleton 
+                    :boxProperties="{
+                        bottom: '10px',
+                        height: '200px'
+                    }"    
+                >
+                </square-skeleton>
             </row>
         </skeleton-loading>
     </div>
--- src/components/basic/squareSkeleton.vue
+++ src/components/basic/squareSkeleton.vue
@@ -1,12 +1,18 @@
 <template>
-    <div
-         class="square"
-         :style="{
-            'backgroundColor': backColor,
-            'width': width,
-			'height': height
-		 }"
-    >
+    <div class="square-list">
+        <div
+             class="square"
+             v-for="i in count"
+             :key="i"
+             :style="{
+                'backgroundColor': backColor,
+                'width': boxProperties.width ?  boxProperties.width : '100%',
+    			'height': boxProperties.height ? boxProperties.height : '16px',
+                'marginTop': boxProperties.top ? boxProperties.top : '0',
+                'marginBottom': boxProperties.bottom ? boxProperties.bottom : '0'
+    		 }"
+        >
+        </div>
     </div>
 </template>
 
@@ -17,18 +23,27 @@
                 type: String,
                 default: '#e7e7e7'
             },
-            width: {
-                type: String,
-                default: "100%"
+            boxProperties: {
+                type: Object,
+                default() {
+                    return {
+                        width: '100%',
+                        height: '16px',
+                        top: '0',
+                        bottom: '0'
+                    };
+                }
             },
-            height: {
-                type: String,
-                default: "16px"
+            count: {
+                type: Number,
+                default: 1
             }
         }
     }
 </script>
 
 <style lang="less">
-
+    .square-list {
+        width: 100%;
+    }
 </style>
